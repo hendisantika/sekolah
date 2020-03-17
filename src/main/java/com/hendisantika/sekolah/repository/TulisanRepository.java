757a0ac5ab2250c3437eaeec0052e12ff419c303
Hendi Santika <hendisantika@gmail.com>
2020-03-18 06:14:49 +0700

    #43 - Add TulisanRepository

--- src/main/java/com/hendisantika/sekolah/repository/TulisanRepository.java
+++ src/main/java/com/hendisantika/sekolah/repository/TulisanRepository.java
@@ -2,7 +2,9 @@ package com.hendisantika.sekolah.repository;
 
 import com.hendisantika.sekolah.entity.Tulisan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -15,4 +17,6 @@ import java.util.UUID;
  * Time: 23.33
  */
 public interface TulisanRepository extends JpaRepository<Tulisan, UUID> {
+    @Query(value = "SELECT t.* FROM tbl_tulisan t ORDER BY t.tulisan_tanggal DESC limit 4;", nativeQuery = true)
+    List<Tulisan> findTop4();
 }
