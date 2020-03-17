de8a51b13fa188f5cf7b28623d48213d0fb14682
Hendi Santika <hendisantika@gmail.com>
2020-03-17 13:16:53 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/sekolah/SekolahApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.sekolah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SekolahApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SekolahApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/sekolah/SekolahApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.sekolah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SekolahApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
