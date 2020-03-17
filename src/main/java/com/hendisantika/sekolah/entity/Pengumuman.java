ca745cee7de50e2025f147d542dfe70d278ea12e
Hendi Santika <hendisantika@gmail.com>
2020-03-18 06:43:37 +0700

    #48 - Add Pengumuman Entity

---
+++ src/main/java/com/hendisantika/sekolah/entity/Pengumuman.java
@@ -0,0 +1,51 @@
+package com.hendisantika.sekolah.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : sekolah
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 18/03/20
+ * Time: 06.41
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "tbl_agenda")
+@EntityListeners(AuditingEntityListener.class)
+public class Pengumuman {
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(name = "agenda_id")
+    private UUID id;
+
+    @Column(name = "pengumuman_judul")
+    private String judul;
+
+    @Column(name = "pengumuman_deskripsi")
+    private String deskripsi;
+
+    @Column(name = "pengumuman_tanggal")
+    private LocalDateTime tanggal;
+
+    @Column(name = "pengumuman_author")
+    @CreatedBy
+    private String author;
+}
