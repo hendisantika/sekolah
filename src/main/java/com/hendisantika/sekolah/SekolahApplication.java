916afb438eb729662e1f53470d5e83ec7467c859
Hendi Santika <hendisantika@gmail.com>
2020-03-17 14:13:55 +0700

    #8 - Enable JPA Auditing in Main App

--- src/main/java/com/hendisantika/sekolah/SekolahApplication.java
+++ src/main/java/com/hendisantika/sekolah/SekolahApplication.java
@@ -2,8 +2,10 @@ package com.hendisantika.sekolah;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SekolahApplication {
 
     public static void main(String[] args) {
