918a98f9d5af1b7a785a0b915b9dc3802608a915
Hendi Santika <hendisantika@gmail.com>
2020-03-17 21:23:30 +0700

    #35 - Add SecurityConfig

---
+++ src/main/java/com/hendisantika/sekolah/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.hendisantika.sekolah.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : sekolah
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/03/20
+ * Time: 21.22
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+}
