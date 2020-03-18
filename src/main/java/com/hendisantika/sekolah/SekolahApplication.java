659ef7f5ff5a1358cf4d96a4a7d04a794f495c9d
Hendi Santika <hendisantika@gmail.com>
2020-03-18 10:09:47 +0700

    #57 - Enable layoutDialect

--- src/main/java/com/hendisantika/sekolah/SekolahApplication.java
+++ src/main/java/com/hendisantika/sekolah/SekolahApplication.java
@@ -1,7 +1,9 @@
 package com.hendisantika.sekolah;
 
+import nz.net.ultraq.thymeleaf.LayoutDialect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -10,6 +12,11 @@ public class SekolahApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SekolahApplication.class, args);
+    }
+
+    @Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
     }
 
 }
