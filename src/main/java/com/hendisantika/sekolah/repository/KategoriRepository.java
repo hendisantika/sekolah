6fabda6ddcb4cec8b49265cba84f6b2edd67a4ec
Hendi Santika <hendisantika@gmail.com>
2020-03-18 14:23:43 +0700

    #68 - Add KategoriRepository

---
+++ src/main/java/com/hendisantika/sekolah/repository/KategoriRepository.java
@@ -0,0 +1,18 @@
+package com.hendisantika.sekolah.repository;
+
+import com.hendisantika.sekolah.entity.Kategori;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : sekolah
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 18/03/20
+ * Time: 14.23
+ */
+public interface KategoriRepository extends JpaRepository<Kategori, UUID> {
+}
