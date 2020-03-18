c3242b7234cb621105598f32de963bf6680e3668
Hendi Santika <hendisantika@gmail.com>
2020-03-18 07:54:18 +0700

    #54 - Add findTop4 Query in PengumumanRepository

--- src/main/java/com/hendisantika/sekolah/repository/PengumumanRepository.java
+++ src/main/java/com/hendisantika/sekolah/repository/PengumumanRepository.java
@@ -2,7 +2,9 @@ package com.hendisantika.sekolah.repository;
 
 import com.hendisantika.sekolah.entity.Pengumuman;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -15,4 +17,6 @@ import java.util.UUID;
  * Time: 06.43
  */
 public interface PengumumanRepository extends JpaRepository<Pengumuman, UUID> {
+    @Query(value = "SELECT p.* FROM tbl_pengumuman p ORDER BY p.pengumuman_tanggal DESC limit 4;", nativeQuery = true)
+    List<Pengumuman> findTop4();
 }
