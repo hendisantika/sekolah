43ee5d2d306f8b071a44e113da01fc79b2297f97
IsmailFajar <ismailfajar81@gmail.com>
2020-03-18 15:43:56 +0700

    fix line InboxRepository

--- src/main/java/com/hendisantika/sekolah/repository/InboxRepository.java
+++ src/main/java/com/hendisantika/sekolah/repository/InboxRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface InboxRepository extends JpaRepository<Inbox, UUID
-        > {
+public interface InboxRepository extends JpaRepository<Inbox, UUID> {
 }
