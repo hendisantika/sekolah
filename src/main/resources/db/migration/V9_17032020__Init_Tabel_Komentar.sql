1e4dc9e6484ed0ea862c190d5088d95fe115786a
Hendi Santika <hendisantika@gmail.com>
2020-03-19 17:56:32 +0700

    #95 - Change column komentar_tulisan_id into UUID in Tabel Komentar

--- src/main/resources/db/migration/V9_17032020__Init_Tabel_Komentar.sql
+++ src/main/resources/db/migration/V9_17032020__Init_Tabel_Komentar.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS tbl_komentar
     komentar_isi        varchar(120)   DEFAULT NULL,
     komentar_tanggal    timestamp NULL DEFAULT CURRENT_TIMESTAMP,
     komentar_status     varchar(2)     DEFAULT NULL,
-    komentar_tulisan_id int            DEFAULT NULL,
+    komentar_tulisan_id UUID           DEFAULT NULL,
     komentar_parent     int            DEFAULT '0',
     PRIMARY KEY (komentar_id)
 );
@@ -17,6 +17,8 @@ CREATE INDEX komentar_tulisan_id_idx ON tbl_komentar (komentar_tulisan_id);
 
 INSERT INTO tbl_komentar (komentar_nama, komentar_email, komentar_isi, komentar_tanggal, komentar_status,
                           komentar_tulisan_id, komentar_parent)
-VALUES ('M Fikri', 'fikrifiver97@gmail.com', ' Nice Post.', '2018-08-07 15:09:07', '1', 24, 0),
-       ('Hendi Santika', 'fikrifiver97@gmail.com', ' Awesome Post', '2018-08-07 15:14:26', '1', 24, 0),
-       ('Joko', 'joko@gmail.com', 'Thank you.', '2018-08-08 03:54:56', '1', 24, 1);
+VALUES ('M Fikri', 'fikrifiver97@gmail.com', ' Nice Post.', '2018-08-07 15:09:07', '1',
+        '2588df34-3058-4faf-a5d2-e8d13aba7e49', 0),
+       ('Hendi Santika', 'fikrifiver97@gmail.com', ' Awesome Post', '2018-08-07 15:14:26', '1',
+        '2588df34-3058-4faf-a5d2-e8d13aba7e49', 0),
+       ('Joko', 'joko@gmail.com', 'Thank you.', '2018-08-08 03:54:56', '1', '2588df34-3058-4faf-a5d2-e8d13aba7e49', 1);
