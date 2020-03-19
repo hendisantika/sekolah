3c4d7bf5367ecfa2481350afd86566cd20e19d4d
Hendi Santika <hendisantika@gmail.com>
2020-03-20 05:34:30 +0700

    #116 - Add MvcConfig

---
+++ src/main/java/com/hendisantika/sekolah/security/MvcConfig.java
@@ -0,0 +1,25 @@
+package com.hendisantika.sekolah.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : sekolah
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/03/20
+ * Time: 05.33
+ */
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("dashboard");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
