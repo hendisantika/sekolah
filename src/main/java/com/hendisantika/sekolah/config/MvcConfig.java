f81eb83f882701834beb6e966e929dd5afb57910
Hendi Santika <hendisantika@gmail.com>
2020-03-20 05:42:27 +0700

    #117 - Add SecurityConfig

--- src/main/java/com/hendisantika/sekolah/config/MvcConfig.java
+++ src/main/java/com/hendisantika/sekolah/config/MvcConfig.java
@@ -1,4 +1,4 @@
-package com.hendisantika.sekolah.security;
+package com.hendisantika.sekolah.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
--- src/main/java/com/hendisantika/sekolah/config/SecurityConfig.java
+++ src/main/java/com/hendisantika/sekolah/config/SecurityConfig.java
@@ -1,13 +1,19 @@
 package com.hendisantika.sekolah.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import static org.springframework.security.config.Customizer.withDefaults;
+import javax.sql.DataSource;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,13 +28,50 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private static final String SQL_LOGIN = "select p.username as username, " +
+            "p.password as password " +
+            "from pengguna p where p.username = ?";
+
+    private static final String SQL_PERMISSION = "select p.username as username, " +
+            "'ROLE_USER' as authority " +
+            "from pengguna p where p.username = ?";
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery(SQL_LOGIN)
+                .authoritiesByUsernameQuery(SQL_PERMISSION)
+                .passwordEncoder(passwordEncoder());
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                 .anyRequest().authenticated()
-                .and().logout().permitAll()
-                .and().formLogin(withDefaults());
+//                .and().logout().permitAll()
+//                .and().formLogin(withDefaults());
+                .and()
+                .formLogin()
+                .failureUrl("/login?error")
+                .loginPage("/login")
+                .defaultSuccessUrl("/")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login")
+                .permitAll();
     }
 
     @Override
