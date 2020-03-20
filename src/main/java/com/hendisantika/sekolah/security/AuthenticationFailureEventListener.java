bc7fee22456791fe62f45de6d2f381108e93700b
Hendi Santika <hendisantika@gmail.com>
2020-03-21 06:38:35 +0700

    #123 - Add AuthenticationFailureEventListener

---
+++ src/main/java/com/hendisantika/sekolah/security/AuthenticationFailureEventListener.java
@@ -0,0 +1,28 @@
+package com.hendisantika.sekolah.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationListener;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : sekolah
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/03/20
+ * Time: 06.37
+ */
+@Component
+@Slf4j
+public class AuthenticationFailureEventListener implements ApplicationListener<AuthenticationFailureBadCredentialsEvent> {
+
+    @Override
+    public void onApplicationEvent(AuthenticationFailureBadCredentialsEvent event) {
+        WebAuthenticationDetails wad = (WebAuthenticationDetails) event.getAuthentication().getDetails();
+        log.info("Login failed request from " + event.getAuthentication().getDetails());
+    }
+
+}
