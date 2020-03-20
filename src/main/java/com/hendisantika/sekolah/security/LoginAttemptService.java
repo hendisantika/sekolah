662b52b88427b7b7a448bbda4e3d58fb73dbb0b7
Hendi Santika <hendisantika@gmail.com>
2020-03-21 06:41:03 +0700

    #126 - Add LoginAttemptService

---
+++ src/main/java/com/hendisantika/sekolah/security/LoginAttemptService.java
@@ -0,0 +1,13 @@
+package com.hendisantika.sekolah.security;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : sekolah
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/03/20
+ * Time: 06.40
+ */
+public class LoginAttemptService {
+}
