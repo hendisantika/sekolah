a7af1bbc5e304f0237bca466ef6472933f2c5951
Hendi Santika <hendisantika@gmail.com>
2020-03-21 06:39:49 +0700

    #124 - Add AuthenticationFailureHandler

---
+++ src/main/java/com/hendisantika/sekolah/security/AuthenticationFailureHandler.java
@@ -0,0 +1,32 @@
+package com.hendisantika.sekolah.security;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : sekolah
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/03/20
+ * Time: 06.39
+ */
+public class AuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException {
+        String errorMessage = "failed login attempt. invalid username or password";
+        if (exception.getMessage().equalsIgnoreCase("blocked")) {
+            errorMessage = "you have been blocked for 3 unsuccessful login attempt";
+        }
+        System.out.println("path " + request.getPathInfo());
+        response.sendRedirect(request.getPathInfo());
+    }
+
+}
