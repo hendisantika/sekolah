4f425328e9f49c0e72da4d3ab1f1dccad86738a2
Hendi Santika <hendisantika@yahoo.co.id>
2020-03-25 19:34:32 +0700

    #172 - Refactor all tables

--- src/main/resources/db/migration/V10_17032020__Init_Tabel_Log_Aktivitas.sql
+++ src/main/resources/db/migration/V10_17032020__Init_Tabel_Log_Aktivitas.sql
@@ -1,13 +1,17 @@
 CREATE TABLE IF NOT EXISTS tbl_log_aktivitas
 (
-    log_id          UUID           DEFAULT uuid_generate_v4() NOT NULL,
-    log_nama        text,
-    log_tanggal     timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-    log_ip          varchar(20)    DEFAULT NULL,
-    log_pengguna_id int            DEFAULT NULL,
-    log_icon        bytea,
-    log_jenis_icon  varchar(50)    DEFAULT NULL,
-    PRIMARY KEY (log_id)
+    id          UUID           DEFAULT uuid_generate_v4() NOT NULL,
+    nama        text,
+    tanggal     timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    ip          varchar(20)    DEFAULT NULL,
+    pengguna_id int            DEFAULT NULL,
+    icon        bytea,
+    jenis_icon  varchar(50)    DEFAULT NULL,
+    created_by  varchar(50),
+    created_on  timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    modified_by varchar(50),
+    modified_on timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
 );
-CREATE INDEX log_pengguna_idx ON tbl_log_aktivitas (log_pengguna_id);
+CREATE INDEX log_pengguna_idx ON tbl_log_aktivitas (pengguna_id);
 -- --------------------------------------------------------
--- src/main/resources/db/migration/V9_17032020__Init_Tabel_Komentar.sql
+++ src/main/resources/db/migration/V9_17032020__Init_Tabel_Komentar.sql
@@ -1,24 +1,27 @@
 CREATE TABLE IF NOT EXISTS tbl_komentar
 (
-    komentar_id         UUID           DEFAULT uuid_generate_v4() NOT NULL,
-    komentar_nama       varchar(30)    DEFAULT NULL,
-    komentar_email      varchar(50)    DEFAULT NULL,
-    komentar_isi        varchar(120)   DEFAULT NULL,
-    komentar_tanggal    timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-    komentar_status     varchar(2)     DEFAULT NULL,
-    komentar_tulisan_id UUID           DEFAULT NULL,
-    komentar_parent     int            DEFAULT '0',
-    PRIMARY KEY (komentar_id)
+    id          UUID           DEFAULT uuid_generate_v4() NOT NULL,
+    nama        varchar(30)    DEFAULT NULL,
+    email       varchar(50)    DEFAULT NULL,
+    isi         varchar(120)   DEFAULT NULL,
+    status      varchar(2)     DEFAULT NULL,
+    tulisan_id  UUID           DEFAULT NULL,
+    parent      int            DEFAULT '0',
+    created_by  varchar(50),
+    created_on  timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    modified_by varchar(50),
+    modified_on timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
 );
-CREATE INDEX komentar_tulisan_id_idx ON tbl_komentar (komentar_tulisan_id);
+CREATE INDEX tulisan_id_idx ON tbl_komentar (tulisan_id);
 --
 -- Dumping data untuk tabel tbl_komentar
 --
 
-INSERT INTO tbl_komentar (komentar_nama, komentar_email, komentar_isi, komentar_tanggal, komentar_status,
-                          komentar_tulisan_id, komentar_parent)
-VALUES ('M Fikri', 'fikrifiver97@gmail.com', ' Nice Post.', '2018-08-07 15:09:07', '1',
+INSERT INTO tbl_komentar (nama, email, isi, created_on, status,
+                          tulisan_id, parent)
+VALUES ('M Fikri', 'fikrifiver97@gmail.com', ' Nice Post.', '2019-08-07 15:09:07', '1',
         '2588df34-3058-4faf-a5d2-e8d13aba7e49', 0),
-       ('Hendi Santika', 'fikrifiver97@gmail.com', ' Awesome Post', '2018-08-07 15:14:26', '1',
+       ('Hendi Santika', 'fikrifiver97@gmail.com', ' Awesome Post', '2019-08-07 15:14:26', '1',
         '2588df34-3058-4faf-a5d2-e8d13aba7e49', 0),
-       ('Joko', 'joko@gmail.com', 'Thank you.', '2018-08-08 03:54:56', '1', '2588df34-3058-4faf-a5d2-e8d13aba7e49', 1);
+       ('Joko', 'joko@gmail.com', 'Thank you.', '2019-08-08 03:54:56', '1', '2588df34-3058-4faf-a5d2-e8d13aba7e49', 1);
