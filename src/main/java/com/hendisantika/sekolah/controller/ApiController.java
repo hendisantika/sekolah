ed1d7b3b4d620b2f3e0b342e4511efe7a3879976
IsmailFajar <ismailfajar81@gmail.com>
2020-03-25 11:49:00 +0700

    add update Kategori to Database

--- src/main/java/com/hendisantika/sekolah/controller/ApiController.java
+++ src/main/java/com/hendisantika/sekolah/controller/ApiController.java
@@ -4,10 +4,13 @@ import com.hendisantika.sekolah.entity.Kategori;
 import com.hendisantika.sekolah.repository.KategoriRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Slf4j
@@ -39,5 +42,15 @@ public class ApiController {
         return kategoriRepository.findAll();
     }
 
+    @PutMapping("/categories/{kategoriId}")
+    @ResponseBody
+    public ResponseEntity<Kategori> updateKategoriFromDB(@PathVariable("kategoriId") UUID kategoriId, @RequestBody @Valid Kategori kategori) {
+        log.info("memperbaharui kategori berita");
+        Optional<Kategori> currentKategoriOpt = kategoriRepository.findById(kategoriId);
+        Kategori currentKategori = currentKategoriOpt.get();
+        currentKategori.setId(kategori.getId());
+        currentKategori.setNama(kategori.getNama());
 
+        return new ResponseEntity<>(kategoriRepository.save(currentKategori), HttpStatus.OK);
+    }
 }
