029db0a4ad1c95a563a09fbdeea65d818eab449f
IsmailFajar <ismailfajar81@gmail.com>
2020-03-25 10:20:16 +0700

    add request mapping

--- src/main/java/com/hendisantika/sekolah/controller/ApiController.java
+++ src/main/java/com/hendisantika/sekolah/controller/ApiController.java
@@ -7,26 +7,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
 @RestController
+@RequestMapping("/v1/api/news")
 public class ApiController {
 
     @Autowired
     private KategoriRepository kategoriRepository;
 
-    @PostMapping("/v1/api/news/categories")
+    @PostMapping("/categories")
     @ResponseBody
     public Kategori addNewKategori(@RequestBody @Valid Kategori kategori) {
         log.info("menambahkan kategori berita");
         return kategoriRepository.save(kategori);
     }
 
-    @GetMapping("/v1/api/news/categories/{kategoriId}")
+    @GetMapping("/categories/{kategoriId}")
     public Kategori findKategoriById(@PathVariable(value = "kategoriId") UUID kategoriId) {
         log.info("filter kategori by id {}", kategoriId);
         Kategori kategori = kategoriRepository.findById(kategoriId).orElse(null);
         return kategori;
     }
+
+    @DeleteMapping("/categories/{kategoriId}")
+    public List<Kategori> deleteKategori(@PathVariable("kategoriId") UUID kategoriId) {
+        log.info("menghapus kategori berita");
+        kategoriRepository.deleteById(kategoriId);
+        return kategoriRepository.findAll();
+    }
+
+
 }
