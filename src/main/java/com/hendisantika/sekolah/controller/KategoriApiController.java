3d546be94942632d602ec05a8409a00281cd0e2e
IsmailFajar <ismailfajar81@gmail.com>
2020-03-25 13:17:48 +0700

    add delete and update Kategori to Database

--- src/main/java/com/hendisantika/sekolah/controller/KategoriApiController.java
+++ src/main/java/com/hendisantika/sekolah/controller/KategoriApiController.java
@@ -5,15 +5,13 @@ import com.hendisantika.sekolah.exception.KategoriNotFoundException;
 import com.hendisantika.sekolah.repository.KategoriRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Slf4j
@@ -39,4 +37,22 @@ public class KategoriApiController {
             return new KategoriNotFoundException("Kategori Not Found");
         });
     }
-}
+
+    @DeleteMapping("/categories/{kategoriId}")
+    public List<Kategori> deleteKategori(@PathVariable("kategoriId") UUID kategoriId) {
+        log.info("menghapus kategori berita");
+        kategoriRepository.deleteById(kategoriId);
+        return kategoriRepository.findAll();
+    }
+
+    @PutMapping("/categories/{kategoriId}")
+    @ResponseBody
+    public ResponseEntity<Kategori> updateKategoriFromDB(@PathVariable("kategoriId") UUID kategoriId, @RequestBody @Valid Kategori kategori) {
+        log.info("memperbaharui kategori berita");
+        Optional<Kategori> currentKategoriOpt = kategoriRepository.findById(kategoriId);
+        Kategori currentKategori = currentKategoriOpt.get();
+        currentKategori.setId(kategori.getId());
+        currentKategori.setNama(kategori.getNama());
+
+        return new ResponseEntity<>(kategoriRepository.save(currentKategori), HttpStatus.OK);
+    }
