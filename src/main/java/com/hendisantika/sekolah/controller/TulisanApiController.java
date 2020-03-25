bbaecf06300e22320eab0f95c1d85fe7e7a9a9b9
Hendi Santika <hendisantika@yahoo.co.id>
2020-03-25 09:42:14 +0700

    #170 - Decorate list Tulisan

--- src/main/java/com/hendisantika/sekolah/controller/TulisanApiController.java
+++ src/main/java/com/hendisantika/sekolah/controller/TulisanApiController.java
@@ -4,8 +4,7 @@ import com.hendisantika.sekolah.repository.TulisanRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.SortDefault;
-import org.springframework.ui.ModelMap;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,10 +25,11 @@ public class TulisanApiController {
     @Autowired
     private TulisanRepository tulisanRepository;
 
-    @GetMapping("/")
-    public String list(ModelMap model, @SortDefault("username") Pageable pageable) {
+    @GetMapping
+//    public String list(ModelMap model, @SortDefault("username") Pageable pageable) {
+    public String list(Model model, Pageable pageable) {
         log.info("List Down Data Tulisan");
-        model.addAttribute("tulisan", tulisanRepository.findAll(pageable));
+        model.addAttribute("tulisanList", tulisanRepository.findAll(pageable));
 
         return "admin/tulisan";
     }
