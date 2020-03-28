e0b05453943897609e74d70a2a9c8ecfacf20863
Hendi Santika <hendisantika@yahoo.co.id>
2020-03-28 13:39:57 +0700

    #198 - Bug Fix getCurrentLogin

--- src/main/java/com/hendisantika/sekolah/config/SystemLoggedInUserAuditorAware.java
+++ src/main/java/com/hendisantika/sekolah/config/SystemLoggedInUserAuditorAware.java
@@ -1,6 +1,11 @@
 package com.hendisantika.sekolah.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -15,10 +20,25 @@ import java.util.Optional;
  * Time: 12.46
  */
 @Component
+@Slf4j
 public class SystemLoggedInUserAuditorAware implements AuditorAware<String> {
 
-    @Override
-    public Optional<String> getCurrentAuditor() {
-        return Optional.of(System.getProperty("user.name"));
-    }
+//  @Override
+//  public Optional<String> getCurrentAuditor() {
+//    return Optional.ofNullable(System.getProperty("user.name"));
+//  }
+
+  @Override
+  public Optional<String> getCurrentAuditor() {
+    Optional<User> user = Optional.ofNullable(SecurityContextHolder.getContext())
+            .map(SecurityContext::getAuthentication)
+            .filter(Authentication::isAuthenticated)
+            .map(Authentication::getPrincipal)
+            .map(User.class::cast);
+    User userLogin = user.get();
+    log.info("Data User {}", userLogin);
+    log.info("getCurrentAuditor {}", userLogin.getUsername());
+    return Optional.ofNullable(userLogin.getUsername());
+
+  }
 }
