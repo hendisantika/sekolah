a8e777bd995248400f7251ba295fe0122f518433
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-03 09:12:44 +0700

    #259 - Process save Tambah Album Form

--- src/main/java/com/hendisantika/sekolah/controller/AlbumController.java
+++ src/main/java/com/hendisantika/sekolah/controller/AlbumController.java
@@ -1,15 +1,29 @@
 package com.hendisantika.sekolah.controller;
 
+import com.hendisantika.sekolah.dto.AlbumDto;
 import com.hendisantika.sekolah.entity.Album;
+import com.hendisantika.sekolah.entity.Pengguna;
 import com.hendisantika.sekolah.repository.AlbumRepository;
+import com.hendisantika.sekolah.repository.PenggunaRepository;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
+import java.io.IOException;
+import java.security.Principal;
+import java.util.Base64;
 
 /**
  * Created by IntelliJ IDEA.
@@ -28,6 +42,9 @@ public class AlbumController {
     @Autowired
     private AlbumRepository albumRepository;
 
+    @Autowired
+    private PenggunaRepository penggunaRepository;
+
     @GetMapping
     public String album(Model model, Pageable pageable) {
         log.info("Menampilkan data untuk Halaman List Album.");
@@ -36,9 +53,40 @@ public class AlbumController {
     }
 
     @GetMapping("add")
-    public String showAlbumForm(Model model, Pageable pageable) {
+    public String showAlbumForm(Model model) {
         log.info("Menampilkan Form Tambah Album.");
-        model.addAttribute("album", new Album());
+        model.addAttribute("album", new AlbumDto());
         return "admin/album/album-form";
+    }
+
+    @PostMapping
+    public String tambahAlbum(@Valid AlbumDto albumDto, Model model, @RequestParam("file") MultipartFile file,
+                              BindingResult errors, Pageable pageable, Principal principal,
+                              SessionStatus status) {
+        log.info("Menambahkan Data Album yang baru");
+        try {
+            String username = principal.getName();
+            Pengguna pengguna = penggunaRepository.findByUsername(username).orElseThrow(() -> {
+                log.warn("Username Not Found {}", username);
+                return new UsernameNotFoundException("Username Not Found");
+            });
+            // Get the file and save it somewhere
+            byte[] bytes = file.getBytes();
+            String encoded = Base64.getEncoder().encodeToString(bytes);
+            Album album = new Album();
+            album.setNama(albumDto.getNama());
+            album.setAuthor(albumDto.getAuthor());
+            album.setPengguna(pengguna);
+            album.setPhotoBase64(encoded);
+            album.setFileContent(bytes);
+            album.setFilename(file.getOriginalFilename());
+            albumRepository.save(album);
+            status.setComplete();
+            log.info("Menambahkan Data guru yang baru sukses.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        model.addAttribute("album", albumRepository.findAll(pageable));
+        return "redirect:/admin/album";
     }
 }
---
+++ src/main/java/com/hendisantika/sekolah/dto/AlbumDto.java
@@ -0,0 +1,22 @@
+package com.hendisantika.sekolah.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : sekolah
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 03/04/20
+ * Time: 08.47
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AlbumDto {
+    private String nama;
+    private String author;
+}
