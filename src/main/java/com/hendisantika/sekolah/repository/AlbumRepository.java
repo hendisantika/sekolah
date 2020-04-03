0ef34559f4d8100def9518e1a9265663ef063644
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-03 07:54:29 +0700

    #252 - Add AlbumRepository

---
+++ src/main/java/com/hendisantika/sekolah/repository/AlbumRepository.java
@@ -0,0 +1,18 @@
+package com.hendisantika.sekolah.repository;
+
+import com.hendisantika.sekolah.entity.Album;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : sekolah
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 03/04/20
+ * Time: 07.53
+ */
+public interface AlbumRepository extends JpaRepository<Album, UUID> {
+}
