256286698d2721ff090009d848fcc5c1dfacebaf
yeahbutstill <mayadani28@gmail.com>
2023-09-16 18:00:23 +0700

    edit fileUtils

--- src/main/java/com/hendisantika/sekolah/util/FileUtils.java
+++ src/main/java/com/hendisantika/sekolah/util/FileUtils.java
@@ -4,6 +4,7 @@ import org.apache.tomcat.util.codec.binary.Base64;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 /**
@@ -20,10 +21,15 @@ public class FileUtils {
         throw new IllegalStateException("Utility class");
     }
 
-    public static String encodeFileToBase64Binary(File file) throws Exception {
-        FileInputStream fileInputStreamReader = new FileInputStream(file);
-        byte[] bytes = new byte[(int) file.length()];
-        fileInputStreamReader.read(bytes);
+    public static String encodeFileToBase64Binary(File file) {
+        byte[] bytes;
+        try (FileInputStream fileInputStreamReader = new FileInputStream(file)) {
+            bytes = new byte[(int) file.length()];
+            fileInputStreamReader.read(bytes);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         return new String(Base64.encodeBase64(bytes, false), StandardCharsets.UTF_8);
     }
 
