569f6b41c604a5a7bc7ffaa3ae26340d8069ca94
yeahbutstill <mayadani28@gmail.com>
2023-09-17 14:02:15 +0700

    rapih-rapih

--- src/main/java/com/hendisantika/sekolah/entity/Files.java
+++ src/main/java/com/hendisantika/sekolah/entity/Files.java
@@ -23,14 +23,15 @@ import java.util.UUID;
  * Email: hendisantika@gmail.com
  * Telegram : @hendisantika34
  * Date: 17/03/20
- * Time: 15.55
+ * Time: 15.19
  */
-@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
 @Entity(name = "tbl_files")
 @EntityListeners(AuditingEntityListener.class)
 public class Files {
@@ -83,40 +84,4 @@ public class Files {
     @Column(name = "modified_on")
     @LastModifiedDate
     private LocalDateTime modifiedOn;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Files files)) return false;
-
-        if (download != files.download) return false;
-        if (!Objects.equals(id, files.id)) return false;
-        if (!Objects.equals(judul, files.judul)) return false;
-        if (!Objects.equals(deskripsi, files.deskripsi)) return false;
-        if (!Objects.equals(author, files.author)) return false;
-        if (!Objects.equals(filename, files.filename)) return false;
-        if (!Arrays.equals(fileContent, files.fileContent)) return false;
-        if (!Objects.equals(data, files.data)) return false;
-        if (!Objects.equals(createdBy, files.createdBy)) return false;
-        if (!Objects.equals(createdOn, files.createdOn)) return false;
-        if (!Objects.equals(modifiedBy, files.modifiedBy)) return false;
-        return Objects.equals(modifiedOn, files.modifiedOn);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (judul != null ? judul.hashCode() : 0);
-        result = 31 * result + (deskripsi != null ? deskripsi.hashCode() : 0);
-        result = 31 * result + (author != null ? author.hashCode() : 0);
-        result = 31 * result + (filename != null ? filename.hashCode() : 0);
-        result = 31 * result + Arrays.hashCode(fileContent);
-        result = 31 * result + download;
-        result = 31 * result + (data != null ? data.hashCode() : 0);
-        result = 31 * result + (createdBy != null ? createdBy.hashCode() : 0);
-        result = 31 * result + (createdOn != null ? createdOn.hashCode() : 0);
-        result = 31 * result + (modifiedBy != null ? modifiedBy.hashCode() : 0);
-        result = 31 * result + (modifiedOn != null ? modifiedOn.hashCode() : 0);
-        return result;
-    }
 }
